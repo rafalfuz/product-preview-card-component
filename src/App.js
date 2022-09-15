8ccddf1bb7059dad229a2b8d499a4b31a87fb330
Rafal Fuz <rafalfuz@gmail.com>
2022-09-15 19:29:33 +0200

    prepared environment to create a project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./theme/GlobalStyles";
+import { theme } from "./theme/mainTheme";
+import styled from "styled-components";
 
-function App() {
+const StyledText = styled.div`
+  font-family: ${({ theme }) => theme.fontFamily.montserrat};
+  font-size: ${({ theme }) => theme.fontSize.l};
+`;
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <StyledText>Hello</StyledText>
+      </ThemeProvider>
+    </>
   );
-}
-
-export default App;
+};
